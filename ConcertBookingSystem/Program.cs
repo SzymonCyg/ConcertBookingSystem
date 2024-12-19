9ba4251d10f1e6ec08cc3246713750f8c487adf0
szymon <mail@example.com>
2024-12-19 23:24:19 +0100

    Ukonczenie klas concert

--- ConcertBookingSystem/Program.cs
+++ ConcertBookingSystem/Program.cs
@@ -1,3 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public class Concert
+{
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public int AvailableSeats { get; set; }
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        this.Name = name;
+        this.Date = date;
+        this.Location = location;
+        this.AvailableSeats = availableSeats;
+    }
+}
+public class Ticket
+{
+    public Concert Concert { get; set; }
+    public int Price { get; set; }
+    public int SeatNumber { get; set; }
+
+    public Ticket(Concert concert, int price, int seatNumber)
+    {
+       this.Concert = concert;
+       this.Price = price;
+       this.SeatNumber = seatNumber;
+    }
+}
+
+public class BookingSystem
+{
+    private List<Concert> concerts = new List<Concert>();
+    private List<Ticket> tickets = new List<Ticket>();
+    
+    public void AddConcert(Concert concert)
+    {
+        concerts.Add(concert);
+    }
+    
+    public bool BookTicket(Concert concert,int price, int seatNumber)
+    {
+        if (concert.AvailableSeats <= 0)
+        {
+            return false; 
+        }
+        else
+        {
+            concert.AvailableSeats--;
+            var ticket = new Ticket(concert, price, seatNumber); 
+            tickets.Add(ticket);
+            return true;   
+        }
+    }
+    public List<Concert> GetConcertsByDate(DateTime date)
+    {
+        return concerts.Where(concert => concert.Date.Date == date.Date).ToList();
+    }
+    
+    public List<Concert> GetConcertsByLocation(string location)
+    {
+        return concerts.Where(concert => concert.Location.Contains(location)).ToList();
+    }
+}
--- ConcertBookingSystem/obj/Debug/net9.0/ConcertBookingSystem.AssemblyInfo.cs
+++ ConcertBookingSystem/obj/Debug/net9.0/ConcertBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConcertBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0333eee42873c4c78476b09dfbff1d5325b756")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConcertBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConcertBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
